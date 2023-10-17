64f50790023159bfb6cc2f79b777a7b35859ccf3
DevGnomino <damianopezze@gmail.com>
2023-10-17 12:04:52 +0200

    added object orientation to slick game (x02_First_Game_with_OO)

--- Design_Patterns/x01_First_Game/src/at/pezze/games/wintergame/MainGame.java
+++ Design_Patterns/x01_First_Game/src/at/pezze/games/wintergame/MainGame.java
@@ -5,7 +5,11 @@ import org.newdawn.slick.*;
 import org.newdawn.slick.tests.AnimationTest;
 
 public class MainGame extends BasicGame {
-    private int x,y;
+    private int rectX, rectY, circleX, circleY, ovalX, ovalY;
+    private int degreeX, degreeY;
+    private int rectDir = 0; // 0:right, 1:down, 2:left, 3:up
+    private int circleDir = 0;
+    private int ovalDir = 0;
 
     public MainGame(String title) {
         super(title);
@@ -14,23 +18,77 @@ public class MainGame extends BasicGame {
     @Override
     public void render(GameContainer gameContainer, Graphics graphics) throws SlickException {
         // wird gezeichnet
-        graphics.drawRect(this.x, this.y, 50, 50);
+        graphics.drawRect(this.rectX, this.rectY, 50, 50);
+        graphics.drawOval(this.circleX, this.circleY, 50, 50);
+        graphics.drawOval(this.ovalX, this.ovalY, 100, 50);
         graphics.setBackground(Color.blue);
     }
 
     @Override
     public void init(GameContainer gameContainer) throws SlickException {
         // 1 mal aufgerufen am Beginn
-        this.x = 50;
-        this.y = 100;
+        this.rectX = 100;
+        this.rectY = 100;
+        this.circleX = 375;
+        this.circleY = 0;
+        this.ovalX = 0;
+        this.ovalY = 400;
     }
 
     @Override
     public void update(GameContainer gameContainer, int delta) throws SlickException {
         // delta = Zeit seit dem letzen Aufruf
-        this.x++;
-        if (this.x >= 700) {
-            this.x = 100;
+
+        //rectangle Movement
+        if (this.rectDir == 0){
+            this.rectX++;
+            if (this.rectX >= 700){
+                this.rectDir = 1;
+            }
+        }
+        else if (this.rectDir == 1){
+            this.rectY++;
+            if (this.rectY >= 500){
+                this.rectDir = 2;
+            }
+        }
+        else if (this.rectDir == 2){
+            this.rectX--;
+            if (this.rectX <= 100){
+                this.rectDir = 3;
+            }
+        }
+        else if (this.rectDir == 3){
+            this.rectY--;
+            if (this.rectY <= 100){
+                this.rectDir = 0;
+            }
+        }
+
+        //circle Movement
+        if (this.circleDir == 0){
+            this.circleY++;
+            if (this.circleY >= 600){
+                this.circleDir = 1;
+            }
+        }
+        else if (this.circleDir == 1){
+            this.circleY = 0;
+            this.circleDir = 0;
+        }
+
+        //oval Movement
+        if (this.ovalDir == 0){
+            this.ovalX++;
+            if (this.ovalX >= 700){
+                this.ovalDir = 1;
+            }
+        }
+        else if (this.ovalDir == 1){
+            this.ovalX--;
+            if (this.ovalX <= 0) {
+                this.ovalDir = 0;
+            }
         }
     }
 
