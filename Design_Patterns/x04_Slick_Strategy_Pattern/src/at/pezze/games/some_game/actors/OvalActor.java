d532a2fa9ae656290e0acb103fa816ccdcf7ab2f
DevGnomino <damianopezze@gmail.com>
2023-10-24 15:16:15 +0200

    Implemented Abstract Class to Slick Game

--- Design_Patterns/x04_Slick_Strategy_Pattern/src/at/pezze/games/some_game/actors/OvalActor.java
+++ Design_Patterns/x04_Slick_Strategy_Pattern/src/at/pezze/games/some_game/actors/OvalActor.java
@@ -1,27 +1,17 @@
 package at.pezze.games.some_game.actors;
 
 import at.pezze.games.some_game.strategies.MoveStrategy;
-import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
 
-public class OvalActor implements Actor {
-    private MoveStrategy moveStrategy;
-
+public class OvalActor extends AbstractActor {
+    //Konstruktor wird gebraucht, da die moveStrategy mit nach unten gegeben werden muss
     public OvalActor(MoveStrategy moveStrategy) {
-        this.moveStrategy = moveStrategy;
-    }
-
-    public void render(Graphics graphics) throws SlickException {
-        graphics.drawOval(this.moveStrategy.getX(), this.moveStrategy.getY(), 45, 25);
+        super(moveStrategy);
     }
 
     @Override
-    public void update(GameContainer gameContainer, int delta) throws SlickException {
-        moveStrategy.update(delta);
+    public void render(Graphics graphics) throws SlickException {
+        graphics.drawOval(this.moveStrategy.getX(), this.moveStrategy.getY(), 45, 25);
     }
-
-    public void init(GameContainer gameContainer) throws SlickException {
-    }
-
 }
--- Design_Patterns/x04_Slick_Strategy_Pattern/src/at/pezze/games/some_game/actors/RectActor.java
+++ Design_Patterns/x04_Slick_Strategy_Pattern/src/at/pezze/games/some_game/actors/RectActor.java
@@ -1,27 +1,17 @@
 package at.pezze.games.some_game.actors;
 
 import at.pezze.games.some_game.strategies.MoveStrategy;
-import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
 
-public class RectActor implements Actor {
-    private MoveStrategy moveStrategy;
-
+public class RectActor extends AbstractActor {
+    //Konstruktor wird gebraucht, da die moveStrategy mit nach unten gegeben werden muss
     public RectActor(MoveStrategy moveStrategy) {
-        this.moveStrategy = moveStrategy;
-    }
-
-    public void render(Graphics graphics) throws SlickException {
-        graphics.drawRect(this.moveStrategy.getX(), this.moveStrategy.getY(), 20, 20);
+        super(moveStrategy);
     }
 
     @Override
-    public void update(GameContainer gameContainer, int delta) throws SlickException {
-        moveStrategy.update(delta);
+    public void render(Graphics graphics) throws SlickException {
+        graphics.drawRect(this.moveStrategy.getX(), this.moveStrategy.getY(), 40, 40);
     }
-
-    public void init(GameContainer gameContainer) throws SlickException {
-    }
-
 }
